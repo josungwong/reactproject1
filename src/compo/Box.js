40c6dfe8982a7308ee5b2868c2b138249aaabc3f
josungwong <jsw13160@gmail.com>
2024-08-14 13:04:27 +0900

    수정완료

--- src/compo/Box.js
+++ src/compo/Box.js
@@ -5,7 +5,7 @@ const Box = (ect) => {
   return (
     <div className='box'>
         <h1>{ect.title}</h1>
-        <img src={ect.item && ect.item.img} alt='none' className='item-img'/>
+        <img src={ect.item && ect.item.img} alt='' className='item-img'/>
         <h2>Win</h2>
     </div>
   )
